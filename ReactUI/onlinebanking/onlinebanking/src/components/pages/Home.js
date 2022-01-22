e30153445e48fdcfbdfbc84608a83d1b9ed70aac
namratha <namrathabanakar111@gmail.com>
2022-01-22 22:26:29 +0530

    second commit

--- ReactUI/onlinebanking/onlinebanking/src/components/pages/Home.js
+++ ReactUI/onlinebanking/onlinebanking/src/components/pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../../App.css';
+import './Home.css';
 
 export default function Home() {
   return (
-    <>
-      <h1 className='home'>OnlineBank</h1>
-    </>
-  );
+    <div>
+      <h1 className='home'></h1>
+      <div className="main">
+            <div className='register'>
+                <form className='login'>
+                <h4>Login</h4>
+                <br/>
+                    <label for="fname">Email Id</label>
+                    <input type="text" id="email" name="email" placeholder="Enter Email Id" />
+
+                    <label for="lname">Password</label>
+                    <input type="text" id="pass" name="pass" placeholder="Enter password" />
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </div>
+    </div> );
 }
