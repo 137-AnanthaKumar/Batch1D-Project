5646f4a9e7a192310f0f88f6c74028db671e8694
kowsi suresh <ananthakumartpy@gmail.com>
2022-01-22 19:04:19 +0530

    react ui

---
+++ ReactUI/onlinebanking/onlinebanking/src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'MobileRecharge',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'CollegeFees',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Electricity',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Investment',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
---
+++ ReactUI/onlinebanking/onlinebanking/src/components/pages/SignUp.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './SignUp.css';
+
+export default function SignUp() {
+  return (<div className="register">
+  <form>
+     
+     <label for="email"><b>Email</b></label>
+     <input type="text" placeholder="Enter Email" name="email" id="email" required />
+     <br></br>
+
+     <label for="email"><b>Email</b></label>
+     <input type="text" placeholder="Enter Email" name="email" id="email" required />
+     <br></br>
+     <label for="email"><b>Email</b></label>
+     <input type="text" placeholder="Enter Email" name="email" id="email" required />
+
+
+  </form>
+  
+  
+  
+  
+  </div>)
+}
