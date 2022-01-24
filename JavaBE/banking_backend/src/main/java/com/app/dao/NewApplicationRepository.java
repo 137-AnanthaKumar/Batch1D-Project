aad420ab373ea24386fedd5d5760db535c82e65b
kowsi suresh <ananthakumartpy@gmail.com>
2022-01-24 21:18:20 +0530

    error solved

--- JavaBE/banking_backend/src/main/java/com/app/dao/NewApplicationRepository.java
+++ JavaBE/banking_backend/src/main/java/com/app/dao/NewApplicationRepository.java
@@ -2,12 +2,14 @@ package com.app.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.app.pojos.NewApplication;
 
-public interface NewApplicationRepository {
+public interface NewApplicationRepository extends JpaRepository<NewApplication ,Long>{
 
-	List<NewApplicationRepository> findAll();
 
-	NewApplicationRepository save(NewApplicationRepository accountopen);
+
+
 
 }
